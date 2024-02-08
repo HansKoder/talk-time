6344102065d3832c381d84992097576250202203
hans.arias <hansarias74@gmail.com>
2024-02-08 14:09:56 -0500

    my first monorepo with talk-time app

---
+++ ms-chat/src/test/java/org/hans/talktime/mschat/MsChatApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hans.talktime.mschat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
